a8db8774f39a7f32170257b2131ade3d86182daf
Guti <gustavogut1993@gmail.com>
2015-02-04 16:31:50 -0430

    Arreglado el 404

--- SAGEParadigm/SAGEParadigm/settings.py
+++ SAGEParadigm/SAGEParadigm/settings.py
@@ -23,6 +23,7 @@ SECRET_KEY = 'd@q$97!ztmy)p&fi5h=-0c_r2rkm6v7qpk@72!taizz+mk2%sw'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 ALLOWED_HOSTS = []
 
@@ -84,3 +85,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
--- SAGEParadigm/SAGEParadigm/urls.py
+++ SAGEParadigm/SAGEParadigm/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'SAGEPhoenix.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^estacionamientos/', include('estacionamientos.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
