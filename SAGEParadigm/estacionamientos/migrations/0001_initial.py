9919f6a85158e6c528a72ed6e168cdb14a8ae418
JoseD92 <jose_daniel_d@hotmail.com>
2015-02-11 19:01:21 -0430

    Preparando para merge

---
+++ SAGEParadigm/estacionamientos/migrations/0001_initial.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Estacionamiento',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('propietario', models.CharField(help_text='Nombre Propio', max_length=50)),
+                ('nombre', models.CharField(max_length=50)),
+                ('direccion', models.TextField(max_length=120)),
+                ('telefono1', models.CharField(blank=True, max_length=30, null=True)),
+                ('telefono2', models.CharField(blank=True, max_length=30, null=True)),
+                ('telefono3', models.CharField(blank=True, max_length=30, null=True)),
+                ('email1', models.EmailField(blank=True, max_length=254, null=True)),
+                ('email2', models.EmailField(blank=True, max_length=254, null=True)),
+                ('rif', models.CharField(max_length=12)),
+                ('object_id', models.PositiveIntegerField()),
+                ('tarifa', models.CharField(blank=True, max_length=50, null=True)),
+                ('apertura', models.TimeField(blank=True, null=True)),
+                ('cierre', models.TimeField(blank=True, null=True)),
+                ('reservasInicio', models.TimeField(blank=True, null=True)),
+                ('reservasCierre', models.TimeField(blank=True, null=True)),
+                ('nroPuesto', models.IntegerField(blank=True, null=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Reserva',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('inicioReserva', models.TimeField()),
+                ('finalReserva', models.TimeField()),
+                ('estacionamiento', models.ForeignKey(to='estacionamientos.Estacionamiento')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='TarifaHora',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('tarifa', models.DecimalField(decimal_places=4, max_digits=10)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='TarifaMinuto',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('tarifa', models.DecimalField(decimal_places=4, max_digits=10)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+    ]
