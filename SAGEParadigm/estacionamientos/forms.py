894deb8e4a077628dc57b009a2815f5527b8852f
adgalad <carlos.25896@gmail.com>
2015-02-18 01:10:55 -0430

    interfaz version 2 hecha

--- SAGEParadigm/estacionamientos/forms.py
+++ SAGEParadigm/estacionamientos/forms.py
@@ -81,7 +81,46 @@ class EstacionamientoReserva(forms.Form):
     final = forms.TimeField(label = 'Horario Final Reserva')
 
 class PagoTarjetaDeCredito(forms.Form):
-    numeroTarjeta = forms.CharField(
+    nombre = forms.CharField( required = True, 
+                            label = "Nombre",
+                            validators = [ RegexValidator( 
+                                       regex = '^[a-zA-ZáéíóúñÑÁÉÍÓÚ][a-zA-ZáéíóúñÑÁÉÍÓÚ ]*$',
+                                       message = 'El apellido no puede iniciar con espacio en blanco ni contener números ni caracteres desconocidos'
+                                       )
+                                    ]
+                            )
+    apellido = forms.CharField( required = True, 
+                            label = "Apellido",
+                            validators = [ RegexValidator( 
+                                       regex = '^[a-zA-ZáéíóúñÑÁÉÍÓÚ][a-zA-ZáéíóúñÑÁÉÍÓÚ ]*$',
+                                       message = 'El nombre no puede iniciar con espacio en blanco ni contener números ni caracteres desconocidos')
+                                    ]
+                            )
+    cedulaTipo = forms.ChoiceField(required = True,
+                                label = 'cedulaTipo',
+                                choices = (
+                                    ('V', 'V'),
+                                    ('E', 'E')
+                                )                       
+                            )
+    cedula = forms.CharField( required = True, 
+                        label = "Cédula",
+                        validators = [ RegexValidator( 
+                                   regex = '^[0-9]+$',
+                                   message = 'La cédula solo puede contener caracteres numéricos')
+                                ]
+                        )
+
+    tarjetaTipo = forms.ChoiceField(required = True,
+                                label = 'tarjetaTipo',
+                                choices = (
+                                    ('Vista', ' VISTA '),
+                                    ('Mister', ' MISTER '),
+                                    ('Xpress', ' XPRESS ')
+                                ),
+                                widget = forms.RadioSelect()
+                            )
+    tarjeta = forms.CharField(
                             required = True,
                             label = "Tarjeta de Credito",
                             validators = [
