f3469ba143b36debb172b2f9993d9d8fcb37a5d3
David Klie <mangok543@gmail.com>
2015-02-19 01:52:13 -0430

    Test de una hora, 7 dias, un dia menos un minuto, un dia, un dia mas un minuto de tarifas por minuto

--- SAGEParadigm/estacionamientos/tests.py
+++ SAGEParadigm/estacionamientos/tests.py
@@ -785,13 +785,43 @@ class RateTestCase(TestCase):
         final_time = datetime.datetime(2015,2,18,15,3)
         rate = TarifaMinuto(tarifa = 60)
         self.assertEqual(rate.calcularPrecio(initial_time,final_time),2)
+        
+    def test_oneHourMinutePay(self):
+        initial_time = datetime.datetime(2015,2,18,15,0)
+        final_time = datetime.datetime(2015,2,18,16,0)
+        rate = TarifaMinuto(tarifa = 60)
+        self.assertEqual(rate.calcularPrecio(initial_time,final_time),60)
 
-    def test_oneDayMinutePay(self):
+    def test_oneDayMinusOneMinuteMinutePay(self):
         initial_time = datetime.datetime(2015,2,18,0,0)
         final_time = datetime.datetime(2015,2,18,23,59)
         rate = TarifaMinuto(tarifa = 60)
         self.assertEqual(rate.calcularPrecio(initial_time,final_time),1439)
+        
+    def test_oneDayMinutePay(self):
+        initial_time = datetime.datetime(2015,2,18,0,0)
+        final_time = datetime.datetime(2015,2,19,0,0)
+        rate = TarifaMinuto(tarifa = 60)
+        self.assertEqual(rate.calcularPrecio(initial_time,final_time),1440)
 
+    def test_oneDayPlusOneMinutePay(self):
+        initial_time = datetime.datetime(2015,2,18,0,0)
+        final_time = datetime.datetime(2015,2,19,0,1)
+        rate = TarifaMinuto(tarifa = 60)
+        self.assertEqual(rate.calcularPrecio(initial_time,final_time),1441)
+        
+    def test_oneDayBeforeMidnightPlusOneMinute(self):
+        initial_time = datetime.datetime(2015,2,18,23,59)
+        final_time = datetime.datetime(2015,2,20,0,0)
+        rate = TarifaMinuto(tarifa = 60)
+        self.assertEqual(rate.calcularPrecio(initial_time,final_time),1441)
+        
+    def test_sevenDays(self):
+        initial_time = datetime.datetime(2015,2,18,23,59)
+        final_time = datetime.datetime(2015,2,25,23,59)
+        rate = TarifaMinuto(tarifa = 60)
+        self.assertEqual(rate.calcularPrecio(initial_time,final_time),7*24*60)
+    
     # Pruebas para la clase tarifa	
 
     def test_OneHourRate(self):
