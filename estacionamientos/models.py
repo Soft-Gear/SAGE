09464d16cc5229ab73822d5f87f33d662b500d80
adgalad <carlos.25896@gmail.com>
2015-02-28 01:31:38 -0430

    detalle

--- estacionamientos/models.py
+++ estacionamientos/models.py
@@ -49,7 +49,7 @@ class Pago(models.Model):
 	monto            = models.DecimalField(decimal_places = 2, max_digits = 256)
 
 	def __str__(self):
-		return str(self.id)
+		return str(self.id)+" "+str(self.reserva.estacionamiento.nombre)+" "+str(self.cedulaTipo)+"-"+str(self.cedula)
 
 class EsquemaTarifario(models.Model):
 
