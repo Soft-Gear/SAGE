a3f06b0cdec7aa3a9fdfb5e8b7f193173f139432
Chiseng <ngchiseng@gmail.com>
2015-05-13 15:52:45 -0430

    Creacion de la BilleteraElectronica en la base de datos. Corrección de error.

--- estacionamientos/admin.py
+++ estacionamientos/admin.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from estacionamientos.models import Estacionamiento, Reserva, Pago, TarifaMinuto,\
-    TarifaHorayFraccion, TarifaHora
+    TarifaHorayFraccion, TarifaHora, BilleteraElectronica
 
 admin.site.register(Estacionamiento)
 admin.site.register(Reserva)
 admin.site.register(Pago)
+admin.site.register(BilleteraElectronica)
 admin.site.register(TarifaHora)
 admin.site.register(TarifaMinuto)
 admin.site.register(TarifaHorayFraccion)
