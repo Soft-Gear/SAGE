0f6130f484414ffbbeadbe133f4cc0aba9c68f0a
Antonio <ragnell93@gmail.com>
2015-06-11 00:56:26 -0430

    Terminado pruebas de forma de estacionamiento extn

--- tests/tests_extended_form.py
+++ tests/tests_extended_form.py
@@ -14,28 +14,28 @@ class ExtendedFormTestCase(TestCase):
 
     # malicia
     def test_estacionamiento_extended_form_un_campo(self):
-        form_data = { 'puestos': 2}
+        form_data = { 'puestos_motos': 2}
         form = EstacionamientoExtendedForm(data = form_data)
         self.assertFalse(form.is_valid())
 
     # malicia
     def test_estacionamiento_extended_form_dos_campos(self):
-        form_data = { 'puestos': 2,
+        form_data = { 'puestos_carros': 2,
                       'horarioin': time(hour = 6,  minute = 0)}
         form = EstacionamientoExtendedForm(data = form_data)
         self.assertFalse(form.is_valid())
 
     # malicia
     def test_estacionamiento_extended_form_tres_campos(self):
-        form_data = { 'puestos': 2,
+        form_data = { 'puestos_autobus': 2,
                       'horarioin': time( hour = 6,  minute = 0),
                       'horarioout': time(hour = 19,  minute = 0)}
         form = EstacionamientoExtendedForm(data = form_data)
         self.assertFalse(form.is_valid())
         
-    # caso borde
+    # Malicia
     def test_estacionamiento_extended_form_cuatro_bien(self):
-        form_data = { 'puestos': 2,
+        form_data = { 'puestos_microbus': 2,
                       'horarioin': time(hour = 6,  minute = 0),
                       'horarioout': time(hour = 19,  minute = 0),
                       'tarifa': '12'
@@ -45,18 +45,30 @@ class ExtendedFormTestCase(TestCase):
 
     # caso borde
     def test_estacionamiento_extended_form_cinco_campos_bien(self):
-        form_data = { 'puestos': 2,
+        form_data = { 'puestos_motos': 2,
                       'horarioin': time(hour = 6,  minute = 0),
                       'horarioout': time(hour = 19,  minute = 0),
                       'tarifa': '12',
                       'esquema':'TarifaMinuto'
                     }
         form = EstacionamientoExtendedForm(data = form_data)
-        self.assertFalse(form.is_valid())
+        self.assertTrue(form.is_valid())
+
+    # caso borde
+    def test_estacionamiento_extended_form_seis_campos_bien(self):
+        form_data = { 'puestos_motos': 2,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'horizonte_reserva' : 7,
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
 
     #TDD
     def test_estacionamiento_extended_form_todos_los_campos_correctos_por_hora(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -64,6 +76,7 @@ class ExtendedFormTestCase(TestCase):
                     'puestos_especiales': 2,
                     'horarioin': time(hour = 6,  minute = 0),
                     'horarioout': time(hour = 19,  minute = 0),
+                    'horizonte_reserva' : 7,
                     'tarifa_motos': '12',
                     'tarifa_carros': '12',
                     'tarifa_autobus': '12',
@@ -77,7 +90,7 @@ class ExtendedFormTestCase(TestCase):
 
     #TDD
     def test_estacionamiento_extended_form_todos_los_campos_correctos_por_minuto(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -98,7 +111,7 @@ class ExtendedFormTestCase(TestCase):
 
     #TDD
     def test_estacionamiento_extended_form_todos_los_campos_correctos_por_hora_fraccion(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -119,7 +132,7 @@ class ExtendedFormTestCase(TestCase):
 
     #TDD
     def test_estacionamiento_extended_form_todos_los_campos_correctos_por_hora_pico(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -133,7 +146,7 @@ class ExtendedFormTestCase(TestCase):
                     'tarifa_microbus': '12',
                     'tarifa_camiones': '12',
                     'tarifa_especiales': '12',
-                    'esquema':'TarifaHoraPico'
+                    'esquema':'TarifaHoraPico',
                     'tarifa2_motos': '12',
                     'tarifa2_carros': '12',
                     'tarifa2_autobus': '12',
@@ -148,7 +161,7 @@ class ExtendedFormTestCase(TestCase):
 
     #TDD
     def test_estacionamiento_extended_form_todos_los_campos_correctos_por_fin_semana(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -162,7 +175,7 @@ class ExtendedFormTestCase(TestCase):
                     'tarifa_microbus': '12',
                     'tarifa_camiones': '12',
                     'tarifa_especiales': '12',
-                    'esquema':'TarifaFinDeSemana'
+                    'esquema':'TarifaFinDeSemana',
                     'tarifa2_motos': '12',
                     'tarifa2_carros': '12',
                     'tarifa2_autobus': '12',
@@ -175,7 +188,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_motos_1(self):
-      form_data = { 'puestos_motos': 1,
+        form_data = { 'puestos_motos': 1,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -196,7 +209,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_carros_1(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 1,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -217,7 +230,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_autobus_1(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 1,
                     'puestos_microbus': 2,
@@ -238,7 +251,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_microbus_1(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 1,
@@ -259,7 +272,7 @@ class ExtendedFormTestCase(TestCase):
         
     #Borde
     def test_estacionamiento_extended_form_puestos_camiones_1(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -280,7 +293,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_especiales_1(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -301,7 +314,7 @@ class ExtendedFormTestCase(TestCase):
 
         #Borde
     def test_estacionamiento_extended_form_puestos_motos_0(self):
-      form_data = { 'puestos_motos': 0,
+        form_data = { 'puestos_motos': 0,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -322,7 +335,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_carros_0(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 0,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -343,7 +356,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_autobus_0(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 0,
                     'puestos_microbus': 2,
@@ -364,7 +377,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_microbus_0(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 0,
@@ -385,7 +398,7 @@ class ExtendedFormTestCase(TestCase):
         
     #Borde
     def test_estacionamiento_extended_form_puestos_camiones_0(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -406,7 +419,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Borde
     def test_estacionamiento_extended_form_puestos_especiales_0(self):
-      form_data = { 'puestos_motos': 2,
+        form_data = { 'puestos_motos': 2,
                     'puestos_carros': 2,
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
@@ -426,8 +439,8 @@ class ExtendedFormTestCase(TestCase):
         self.assertTrue(form.is_valid())
 
      #Esquina
-    def test_estacionamiento_extended_form_solo_un_puesto_carros(self):
-      form_data = { 'puestos_motos': 0,
+    def test_estacionamiento_extended_form_solo_un_puesto(self):
+        form_data = { 'puestos_motos': 0,
                     'puestos_carros': 1,
                     'puestos_autobus': 0,
                     'puestos_microbus': 0,
@@ -448,7 +461,7 @@ class ExtendedFormTestCase(TestCase):
 
     #Esquina
     def test_estacionamiento_extended_form_ningun_puesto(self):
-      form_data = { 'puestos_motos': 0,
+        form_data = { 'puestos_motos': 0,
                     'puestos_carros': 0,
                     'puestos_autobus': 0,
                     'puestos_microbus': 0,
@@ -465,7 +478,7 @@ class ExtendedFormTestCase(TestCase):
                     'esquema':'TarifaHorayFraccion'
                     }
         form = EstacionamientoExtendedForm(data = form_data)
-        self.assertFalse(form.is_valid())
+        self.assertTrue(form.is_valid())
 
     # caso borde
     def test_estacionamiento_extended_form_hora_inicio_igual_hora_cierre(self):
@@ -474,7 +487,7 @@ class ExtendedFormTestCase(TestCase):
                     'puestos_autobus': 2,
                     'puestos_microbus': 2,
                     'puestos_camiones': 2,
-                    'puestos_especiales': 0,
+                    'puestos_especiales': 2,
                     'horarioin': time(hour = 6,  minute = 0),
                     'horarioout': time(hour = 6,  minute = 0),
                     'tarifa_motos': '12',
@@ -538,7 +551,7 @@ class ExtendedFormTestCase(TestCase):
                     'puestos_microbus': 2,
                     'puestos_camiones': 2,
                     'puestos_especiales': 2,
-                    'horarioin': "hola",
+                    'horarioin': time(hour = 6,  minute = 0),
                     'horarioout': time(hour = 19,  minute = 0),
                     'tarifa_motos': None,
                     'tarifa_carros': None,
@@ -549,4 +562,24 @@ class ExtendedFormTestCase(TestCase):
                     'esquema':'TarifaHorayFraccion'
                     }
         form = EstacionamientoExtendedForm(data = form_data)
-        self.assertFalse(form.is_valid())
+        self.assertTrue(form.is_valid())
+
+    #Borde
+    def test_estacionamiento_extended_form_horizonte_minimo(self):
+        form_data = { 'horizonte_reserva' : 0,
+                    'horarioin': time(hour = 6,  minute = 0),
+                    'horarioout': time(hour = 19,  minute = 0),
+                    'esquema':'TarifaHorayFraccion'
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
+
+    #Borde
+    def test_estacionamiento_extended_form_horizonte_maximo(self):
+        form_data = { 'horizonte_reserva' : 15,
+                    'horarioin': time(hour = 6,  minute = 0),
+                    'horarioout': time(hour = 19,  minute = 0),
+                    'esquema':'TarifaHorayFraccion'
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
